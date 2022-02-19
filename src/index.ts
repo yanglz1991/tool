6b47ba9cdc07269871c11681fc580acb47d3e5e2
yanglzh <lgm122@qq.com>
2022-02-19 23:05:59 +0800

    0.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
  * @description 返回url中的参数对象
  * @returns {Object}
  */
-export function getParamsObj() {
-  const paramsObj = {}
+export function getParamsObj(): any {
+  const paramsObj: any = {}
   if (window.location.search) {
     const paramsArr = window.location.search.substr(1).split('&')
     paramsArr.forEach((param) => {
@@ -13,5 +13,3 @@ export function getParamsObj() {
   }
   return paramsObj
 }
-
-export default { getParamsObj }
